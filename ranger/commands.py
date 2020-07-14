1180cb62b4ee9e76bb43926723ac51dc396d23a2
diaojian <13501811024@163.com>
2020-07-14 12:24:25 +0800

    ranger

--- ranger/commands.py
+++ ranger/commands.py
@@ -2,19 +2,13 @@ from ranger.api.commands import Command
 import os
 from ranger.core.loader import CommandLoader
 
-
 class paste_as_root(Command):
-    def execute(self):
-        if self.fm.do_cut:
-            self.fm.execute_console('shell sudo mv %c .')
-        else:
-            self.fm.execute_console('shell sudo cp -r %c .')
+	def execute(self):
+		if self.fm.do_cut:
+			self.fm.execute_console('shell sudo mv %c .')
+		else:
+			self.fm.execute_console('shell sudo cp -r %c .')
 
-
-class toggleVCS(Command):
-    def execute(self):
-        self.fm.execute_console("set vcs_aware!")
-        self.fm.execute_console("reload_cwd")
 
 
 class mkcd(Command):
@@ -23,6 +17,7 @@ class mkcd(Command):
 
     Creates a directory with the name <dirname> and enters it.
     """
+
     def execute(self):
         from os.path import join, expanduser, lexists
         from os import makedirs
@@ -39,8 +34,7 @@ class mkcd(Command):
 
             for m in re.finditer('[^/]+', dirname):
                 s = m.group(0)
-                if s == '..' or (s.startswith('.')
-                                 and not self.fm.settings['show_hidden']):
+                if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
                     self.fm.cd(s)
                 else:
                     ## We force ranger to load content before calling `scout`.
@@ -48,7 +42,6 @@ class mkcd(Command):
                     self.fm.execute_console('scout -ae ^{}$'.format(s))
         else:
             self.fm.notify("file/directory exists!", bad=True)
-
 
 class fzf_select(Command):
     """
@@ -65,17 +58,13 @@ class fzf_select(Command):
         import os.path
         if self.quantifier:
             # match only directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-
         else:
             # match files and directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-
-        fzf = self.fm.execute_command(command,
-                                      universal_newlines=True,
-                                      stdout=subprocess.PIPE)
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
@@ -83,6 +72,8 @@ class fzf_select(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
+
+
 
 
 class compress(Command):
@@ -113,11 +104,7 @@ class compress(Command):
         """ Complete with current folder name """
 
         extension = ['.zip', '.tar.gz', '.rar', '.7z']
-        return [
-            'compress ' + os.path.basename(self.fm.thisdir.path) + ext
-            for ext in extension
-        ]
-
+        return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
 
 class extracthere(Command):
     def execute(self):
@@ -143,10 +130,12 @@ class extracthere(Command):
         if len(copied_files) == 1:
             descr = "extracting: " + os.path.basename(one_file.path)
         else:
-            descr = "extracting files from: " + os.path.basename(
-                one_file.dirname)
+            descr = "extracting files from: " + os.path.basename(one_file.dirname)
         obj = CommandLoader(args=['aunpack'] + au_flags \
                 + [f.path for f in copied_files], descr=descr, read=True)
 
         obj.signal_bind('after', refresh)
         self.fm.loader.add(obj)
+
+
+
